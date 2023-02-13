059ec320ac0155244c7ad8f551be2f40c5f09b61
drkop <satanatyn@yandex.ru>
2023-02-13 19:35:52 +0300

    4

---
+++ 4Ex/Program.cs
@@ -0,0 +1,10 @@
+﻿int answer = 0;
+int [] numbers = new int [5];
+for (int i = 0; i < numbers.Length; i++)
+{ numbers[i] = new Random().Next(0, 100);}
+for (int i = 0; i < numbers.Length; i++)
+ {Console.Write(numbers[i] + "|");}
+for (int i = 0; i < numbers.Length; i++)
+{ if (i%2!=0) answer = numbers[i] + answer; }
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел на нечетных позициях равна {answer}");
