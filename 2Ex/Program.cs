5b4ca0f6301cc498ed5967f7984839af04ec7b3a
drkop <satanatyn@yandex.ru>
2023-02-13 18:44:18 +0300

    2

---
+++ 2Ex/Program.cs
@@ -0,0 +1,9 @@
+﻿int [] numbers = new int [8];
+for (int i = 0; i < numbers.Length; i++)
+{ numbers[i] = new Random().Next(-100, 100);}
+ for (int i = 0; i < numbers.Length; i++)
+ {Console.Write(numbers[i] + "|");}
+int a = 0; int b = 0;
+for (int i = 0; i < numbers.Length; i++)
+{if (numbers[i]%2==0) a = a+1;else b = b+1;}
+Console.WriteLine($"Количество четных чисел равно {a}, а количество нечетных - {b}");
